295249fbf496ad893a0697e48f029aaccc2fdd7b
Mibuchi-0308 <76603585+Mibuchi-0308@users.noreply.github.com>
2021-01-21 14:03:44 +0900

    ログインモーダル原型作成&ヘッダーメニューのデザイン調整

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -3,35 +3,60 @@ import PropTypes from "prop-types"
 class Login extends React.Component {
   constructor(props) {
   super(props);
-  this.state = {isModalOpen: false};
+  this.state = {
+    isModalOpen: false,
+    isTestOpen: false,
+  };
   }
 
-  hanldeClickLogin() {
+  handleClickOpen() {
     this.setState({isModalOpen: true});
   }
 
   handleClickClose() {
-    this.setState({isModalOpen: false})
+    this.setState({isModalOpen: false});
   }
+
+  //handleClickTestOpen() {
+    //this.setState({isTestOpen: !this.state.isTestOpen})
+  //}
 
   render() {
     let modal;
-    if (this.state.isModalOpen)
-    modal = (
-      <div className="login-modal">
-        <h2>ログイン</h2>
-          <ul>
-            <li>メールアドレス</li>
-            <li><input /></li>
-            <li>パスワード</li>
-            <li><input /></li>
-          </ul>
-          <button>新規登録</button>
-          <button>ログイン</button>
-      </div>
-    );
+    if (this.state.isModalOpen) {
+      modal = (
+        <div className="login-modal">
+          <h2>ログイン</h2>
+            <ul>
+              <li>メールアドレス</li>
+              <li><input /></li>
+              <li>パスワード</li>
+              <li><input /></li>
+            </ul>
+            <button>新規登録</button>
+            <button>ログイン</button>
+            <button onClick={() => {this.handleClickClose()}}>
+              とじる
+            </button>
+        </div>
+      );}
+
+    //let message;
+    //if (this.state.isTestOpen) {
+      //message = (
+        //<p>機能しちょる</p>
+      //);
+    //}
+
     return (
-      <p>鯖の味噌煮です</p>
+      <div>
+        <div
+          className="hs-element"
+          onClick={() => {this.handleClickOpen()}}>
+          <p>ログイン</p>
+        </div>
+        {modal}
+      </div>
     );
   }
 }
