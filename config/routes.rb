abbe2423631482a4f143be21a8b6ea75e205319d
Mibuchi-0308 <76603585+Mibuchi-0308@users.noreply.github.com>
2021-01-26 18:42:40 +0900

    Folderのコントローラー作成。DBをPostgresqlに変更する準備と複数データの受け渡しを別プロジェクトにてテスト中。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'folders/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get '/' => "home#top"
+    get "/" => "home#top"
     get "/howto" => "home#howto"
+    get "/folders" => "folders#index"
 end
