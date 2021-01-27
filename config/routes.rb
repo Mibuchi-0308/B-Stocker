1b3a896b5eef00284e6ee8346ce7076e76793e8a
Mibuchi-0308 <76603585+Mibuchi-0308@users.noreply.github.com>
2021-01-27 23:57:20 +0900

    フォルダ部分制作中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'folders/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get "/" => "home#top"
-    get "/howto" => "home#howto"
-    get "/folders" => "folders#index"
+  get "/" => "home#top"
+  get "/howto" => "home#howto"
+  get "/folders" => "folders#index"
 end
