688f5a62c39cb7da0acdbdca7e24577434238d85
Mibuchi-0308 <76603585+Mibuchi-0308@users.noreply.github.com>
2021-03-23 15:11:32 +0900

    ログイン権限の検証アクションを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,26 @@ class ApplicationController < ActionController::Base
   def set_passwordChecked
     @checkedPassword = session[:userPassword]
   end
+
+  def authenticate_user
+    if @currentUser == nil
+      flash[:notice] = "ログインをしてください"
+      redirect_to("/")
+    end
+  end
+
+  def forbid_login_user
+    if @currentUser
+      flash[:notice] = "既にログインしています"
+      redirect_to("/users/#{@cuurentUser/id}")
+    end
+  end
+
+  def ensure_correct_user
+    if @currentUser != params[:user_id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/menu")
+    end
+  end
+
 end
